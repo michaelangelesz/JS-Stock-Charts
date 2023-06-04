55353df0d6aa1719e32c0f54ee1fcbf968c22547
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-04 13:21:29 -0400

    Update chart config to use custom colors for each
    added getColor function to set the color properties for each dataset in the array, based on stock symbol. This makes it easier to identify which line corresponds to which stock on the chart.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,18 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
@@ -19,8 +34,8 @@ async function main() {
             labels: stocks[0].values.map(value => value.datetime),
             datasets: stocks.map( stock => ({
                 label: stock.meta.symbol,
-                backgroundColor:  'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol), 
                 data: stock.values.map(value => parseFloat(value.high))
             }))
         }
