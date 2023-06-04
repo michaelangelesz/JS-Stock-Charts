b39e4d38a17433f403f390ba20cef6e7a338d82c
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-04 11:07:23 -0400

    Write fetch request
    write a fetch request based on twelvedata's documentation, parse response from JSON, use console.log to inspect the data

--- public/index.js
+++ public/index.js
@@ -4,6 +4,15 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=cfe59fb569034896ad0b9abfc70c2f1b`)
+
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    console.log(result)
 }
 
 main()
