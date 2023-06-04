c4128521d61a58ab753d1a1a6bde58af6d1c16b8
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-04 12:06:02 -0400

    Add constructor function call for Chart.js
    This commit adds a constructor function call for Chart.js to create a new instance of the Chart object and create a bar chart on the web page.

--- public/index.js
+++ public/index.js
@@ -12,7 +12,23 @@ async function main() {
 
     const stocks = [GME, MSFT, DIS, BNTX];
 
-    console.log(Chart)
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+            }]
+        }
+    });
+    
 }
 
 main()
