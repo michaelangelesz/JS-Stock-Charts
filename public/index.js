0bc4a9dd07113b4e7308b86499f299da7262e7da
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-04 16:16:25 -0400

    ((READ NOTE!)) Final format of code
    adding the calculateAverage function breaks the whole page. When I console.log using mockData (commenting out my fetch request) the stocks show in the console just fine. My API is fresh. So since twelvedata only allows 8 fetches per min, and this page shows three charts that use 4 stocks each will make 12 fetches. hence breaking the code

--- public/index.js
+++ public/index.js
@@ -1,104 +1,104 @@
-function getColor(stock){
-    if(stock === "GME"){
-        return 'rgba(61, 161, 61, 0.7)'
-    }
-    if(stock === "MSFT"){
-        return 'rgba(209, 4, 25, 0.7)'
-    }
-    if(stock === "DIS"){
-        return 'rgba(18, 4, 209, 0.7)'
-    }
-    if(stock === "BNTX"){
-        return 'rgba(166, 43, 158, 0.7)'
-    }
+function getColor(stock) {
+  if (stock === "GME") {
+    return "rgba(61, 161, 61, 0.7)";
+  }
+  if (stock === "MSFT") {
+    return "rgba(209, 4, 25, 0.7)";
+  }
+  if (stock === "DIS") {
+    return "rgba(18, 4, 209, 0.7)";
+  }
+  if (stock === "BNTX") {
+    return "rgba(166, 43, 158, 0.7)";
+  }
 }
 
 async function main() {
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 
-    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=1b2d091a1ecd404ab9dfcbd862828a3f`)
+  //const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=1b2d091a1ecd404ab9dfcbd862828a3f`)
 
-    const result = await response.json()
+  const result = await response.json();
 
-    const { GME, MSFT, DIS, BNTX } = result;
+  const { GME, MSFT, DIS, BNTX } = mockData;
 
-    const stocks = [GME, MSFT, DIS, BNTX];
+  const stocks = [GME, MSFT, DIS, BNTX];
 
-    stocks.forEach( stock => stock.values.reverse())
+  stocks.forEach((stock) => stock.values.reverse());
 
-    /** Time (line chart) */
-    new Chart(timeChartCanvas.getContext('2d'), {
-        type: 'line',
-        data: {
-            labels: stocks[0].values.map(value => value.datetime),
-            datasets: stocks.map( stock => ({
-                label: stock.meta.symbol,
-                backgroundColor:  getColor(stock.meta.symbol),
-                borderColor: getColor(stock.meta.symbol), 
-                data: stock.values.map(value => parseFloat(value.high))
-            }))
-        }
-    });    
-    
-    /** High (bar chart) */
-    new Chart(highestPriceChartCanvas.getContext('2d'), {
-        type: 'bar',
-        data: {
-            labels: stocks.map(stock => stock.meta.symbol),
-            datasets: [{
-                label: 'Highest',
-                backgroundColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
-                )),
-                borderColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
-                )),
-                data: stocks.map(stock => (
-                    findHighest(stock.values) 
-                )) 
-            }]
-        }
-    });
+  /** Time (line chart) */
+  new Chart(timeChartCanvas.getContext("2d"), {
+    type: "line",
+    data: {
+      labels: stocks[0].values.map((value) => value.datetime),
+      datasets: stocks.map((stock) => ({
+        label: stock.meta.symbol,
+        backgroundColor: getColor(stock.meta.symbol),
+        borderColor: getColor(stock.meta.symbol),
+        data: stock.values.map((value) => parseFloat(value.high)),
+      })),
+    },
+  });
 
-     /** Average (pie chart) */
-     new Chart(averagePriceChartCanvas.getContext('2d'), {
-        type: 'pie',
-        data: {
-            labels: stocks.map(stock => stock.meta.symbol),
-            datasets: [{
-                label: 'Average', 
-                backgroundColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
-                )),
-                borderColor: stocks.map(stock => (
-                    getColor(stock.meta.symbol)
-                )), 
-                data: stocks.map(stock => (
-                    calculateAverage(stock.values)
-                )) 
-            }] 
-        } 
-    });
+  /** High (bar chart) */
+  new Chart(highestPriceChartCanvas.getContext("2d"), {
+    type: "bar",
+    data: {
+      labels: stocks.map((stock) => stock.meta.symbol),
+      datasets: [
+        {
+          label: "Highest",
+          backgroundColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          borderColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          data: stocks.map((stock) => findHighest(stock.values)),
+        },
+      ],
+    },
+  });
+
+  /** Average (pie chart) */
+  new Chart(averagePriceChartCanvas.getContext("2d"), {
+    type: "pie",
+    data: {
+      labels: stocks.map((stock) => stock.meta.symbol),
+      datasets: [
+        {
+          label: "Average",
+          backgroundColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          borderColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          data: stocks.map((stock) => calculateAverage(stock.values)),
+        },
+      ],
+    },
+  });
 }
 
 function findHighest(values) {
-    let highest = 0;
-    values.forEach(value => {
-        if (parseFloat(value.high) > highest) {
-            highest = value.high
-        }
-    })
-    return highest
-} 
+  let highest = 0;
+  values.forEach((value) => {
+    if (parseFloat(value.high) > highest) {
+      highest = value.high;
+    }
+  });
+  return highest;
+}
 
 function calculateAverage(values) {
-    let total = 0;
-    values.forEach(value => {
-        total += parseFloat(value.high)
-    })
-    return total / values.length
-} 
+  let total = 0;
+  values.forEach((value) => {
+    total += parseFloat(value.high);
+  });
+  return total / values.length;
+}
 
-main() 
+console.log(mockData);
+
+main();
+
+/** adding the calculateAverage function breaks the whole page. When I console.log using mockData (commenting out my fetch request) the stocks show in the console just fine. My API is fresh. So since twelvedata only allows 8 fetches per min, and this page shows three charts that use 4 stocks each will make 12 fetches. hence breaking the code */
